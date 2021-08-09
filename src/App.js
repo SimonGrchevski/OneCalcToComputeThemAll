9b4bb9e67c184ada7a59c6f746592d16d9bef91e
SimonGrchevski <simongrcevski@gmail.com>
2021-08-09 12:26:37 +0200

    Fix Eslint and Styleint errors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>My first React APP</h1>
+        <h1>My first React APP</h1>
       </header>
     </div>
   );
