8c961398c8985bb8d86c5a7f39b6a6e832bc3608
SimonGrchevski <simongrcevski@gmail.com>
2021-08-14 11:39:59 +0200

    Refact Input into a functional component

--- src/components/input.js
+++ src/components/input.js
@@ -1,29 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Input extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+export default function Input(props) {
+  const { display } = props;
 
-  handleChange = (e) => {
-    const { display } = this.props;
+  function handleChange(e) {
     e.target.value = display;
   }
 
-  render() {
-    const { display } = this.props;
-    return (
-      <input type="text" className="calc-inp" value={display} onChange={this.handleChange} />
-    );
-  }
+  return (
+    <input type="text" className="calc-inp" value={display} onChange={handleChange} />
+  );
 }
 
 Input.propTypes = {
   display: PropTypes.string.isRequired,
-};
-
-Input.defaultProp = {
-  value: '0',
 };
